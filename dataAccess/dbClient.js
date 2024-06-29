cd2743bf4978bcd1d843971c6ee662cd8f0c09a7
kvitee <kvitee41@gmail.com>
2024-06-29 15:17:31 +0800

    Create dataAccess module.
    Module contains the logic to create and provide a single DB client
    to make SQL queries.

---
+++ dataAccess/dbClient.js
@@ -0,0 +1,13 @@
+import pg from "pg";
+
+
+const dbClient = new pg.Client();
+
+try {
+  await dbClient.connect();
+
+  console.log("Client connected successfully.");
+} catch (error) {
+  console.error(error);
+  console.warn("Client is not ready to use.");
+}
