6590edfcbf17f9be77eb9665596206cf5edb27a2
kvitee <kvitee41@gmail.com>
2024-07-06 23:59:47 +0800

    Change UserService to use typeorm to access data.

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,25 +1,63 @@
-import { UserModel } from "../models/UserModel.js";
+import { User } from "../models/User.js";
+import { userRepository } from "../dataAccess/userRepository.js";
+
+import { UserNotExistError } from "../exceptions/UserNotExist.js";
+import { PropertyRequiredError } from "../exceptions/PropertyRequired.js";
 
 
 class UserService {
   static async create({name, age}) {
-    return await UserModel.create({ name, age });
+    const user = new User();
+
+    user.name = name;
+    user.age = age;
+
+    this.validateUser(user);
+
+    await userRepository.save(user);
+
+    return user.id;
   }
 
   static async getAll() {
-    return await UserModel.getAll();
+    return await userRepository.find();
   }
 
   static async get(id) {
-    return await UserModel.get(id);
+    const user = await userRepository.findOneBy({ id });
+
+    if (user === null) {
+      throw new UserNotExistError(id);
+    }
+
+    return user;
   }
 
   static async update(id, {name, age}) {
-    return await UserModel.update(id, { name, age });
+    const user = await this.get(id);
+
+    user.name = name;
+    user.age = age;
+
+    this.validateUser(user);
+
+    await userRepository.save(user);
   }
 
   static async delete(id) {
-    return await UserModel.delete(id);
+    await userRepository.remove(
+      await this.get(id)
+    );
+  }
+
+  static validateUser(user) {
+    if (!user.name) {
+      throw new PropertyRequiredError("name");
+    }
+
+    if (!user.age) {
+      throw new PropertyRequiredError("age");
+    }
   }
 };
 
